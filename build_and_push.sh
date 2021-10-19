d722f2dc8f80dddd19f8238390b11d3a7c1d272a
aswinnarayanan <aswinn89@gmail.com>
2021-10-19 05:45:27 +0000

    Rework repo structure for repeat builds

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-repo2docker --no-run --image-name aswinnarayanan/jupyterserverproxy-neurodesk:latest .
-docker push aswinnarayanan/jupyterserverproxy-neurodesk:latest
+timestamp=$(date +%Y%m%d%H%M%S)
+
+repo2docker --no-run --image-name aswinnarayanan/jupyterserverproxy-neurodesk:$timestamp r2d
+
+docker push aswinnarayanan/jupyterserverproxy-neurodesk:$timestamp
+echo "FROM aswinnarayanan/jupyterserverproxy-neurodesk:$timestamp" > binder/Dockerfile
