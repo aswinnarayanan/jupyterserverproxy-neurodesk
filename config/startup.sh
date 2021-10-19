d2617b0ebdc3fe628646e439267befc5eef66353
aswinnarayanan <aswinn89@gmail.com>
2021-10-19 10:22:17 +0000

    Set manual guacd startup

--- config/startup.sh
+++ config/startup.sh
@@ -4,7 +4,7 @@
 # sudo service xrdp restart
 
 # USER=jovyan vncserver -kill :1
-USER=jovyan vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" :1
+# USER=jovyan vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" :1
 
 /home/jovyan/tomcat/bin/startup.sh
-guacd -L debug -f &
+# guacd -L debug -f
